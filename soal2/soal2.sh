9096eac58ef7b9095d9d9999f6c6e995836375ac
Kevin Christian Hadinata <38512540+kevinc45@users.noreply.github.com>
2020-02-21 15:13:47 +0700

    Create soal2.sh

---
+++ soal2/soal2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+A=$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 28 ; echo '')
+B=$(echo $1 | tr -dc A-Za-z)
+y=abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
+z=ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
+x=$((`date +%I`))
+newB=$(echo $B | tr "${z:0:26}${y:0:26}" "${z:$x:26}${y:$x:26}")
+echo $A>$newB.txt
