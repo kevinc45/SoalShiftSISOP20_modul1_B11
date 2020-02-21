17689953f9c4afd8749c70154eeb68fc005c8d88
Kevin Christian Hadinata <38512540+kevinc45@users.noreply.github.com>
2020-02-21 15:59:31 +0700

    Update soal2.sh

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -4,6 +4,6 @@ A=$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 28 ; echo '')
 B=$(echo $1 | tr -dc A-Za-z)
 y=abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
 z=ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
-x=$((`date +%I`))
+x=$((`date +%H`))
 newB=$(echo $B | tr "${z:0:26}${y:0:26}" "${z:$x:26}${y:$x:26}")
 echo $A>$newB.txt
